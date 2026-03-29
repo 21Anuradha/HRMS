4aee1da473ff95ee9ef14d9c9740e4a9ad1d0128
Ram <rampujan1999@gmail.com>
2026-03-29 23:15:35 +0530

    Initial project upload

---
+++ src/main/java/in/hrms/core/HrmsCoreApplication.java
@@ -0,0 +1,13 @@
+package in.hrms.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/hrms/core/HrmsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hrms.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
